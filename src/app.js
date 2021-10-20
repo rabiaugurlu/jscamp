074b7d459e34fc1342bd096a71eabaad48c2a35b
Rabia Ugurlu <rabia_ugurluu@hotmail.com>
2021-10-20 13:26:02 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("merhaba kodlama.io");
+
+let dolarDun = 9.20;
+let dolarBugun = 9.50;
+
+console.log(dolarDun);
+
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi", "Kamu konut kredisi","Özel konut kredileri"]
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
